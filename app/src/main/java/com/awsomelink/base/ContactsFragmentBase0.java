dc84ea0474fed643c3bec2606bcd4d288a985834
maksud.nurullaev@gmail.com <maksud.nurullaev@gmail.com>
2015-04-01 15:48:06 +0500

    Stable list view for contacts with phones

--- app/src/main/java/com/awsomelink/base/ContactsFragmentBase0.java
+++ app/src/main/java/com/awsomelink/base/ContactsFragmentBase0.java
@@ -4,7 +4,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
@@ -16,7 +15,6 @@ import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.awsomelink.R;
@@ -40,14 +38,9 @@ public class ContactsFragmentBase0 extends ListFragment
 
 
     // These are the Contacts rows that we will retrieve
-    // static final String[] PROJECTION = new String[] {ContactsContract.Data._ID, ContactsContract.Data.DISPLAY_NAME};
-    // static final String[] PROJECTION = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME };
     static final String[] CONTACTS_SUMMARY_PROJECTION = new String[]{
             Contacts._ID,
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME,
-            Contacts.CONTACT_STATUS,
-            Contacts.CONTACT_PRESENCE,
-            Contacts.PHOTO_ID,
             Contacts.LOOKUP_KEY,
     };
 
@@ -73,74 +66,16 @@ public class ContactsFragmentBase0 extends ListFragment
         updateHeader();
     }
 
-    // This is the select criteria
-/*
-    static final String SELECTION = "((" +
-            ContactsContract.Data.DISPLAY_NAME + " NOTNULL) AND (" +
-            ContactsContract.Data.DISPLAY_NAME + " != '' ))";
-*/
-    static final String SELECTION = ContactsContract.Contacts.DISPLAY_NAME + " <> \"\" " ;
-    static final String ORDER     = "UPPER(" + ContactsContract.Contacts.DISPLAY_NAME + ") ASC" ;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        // Create a progress bar to display while the list loads
-/*
-        ProgressBar progressBar = new ProgressBar(getActivity());
-        progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-        progressBar.setIndeterminate(true);
-        getListView().setEmptyView(progressBar);
-*/
-        // Must add the progress bar to the root of the layout
-//        ViewGroup root = (ViewGroup) getActivity().findViewById(android.R.id.content);
-//        root.addView(progressBar);
-
-        // For the cursor adapter, specify which columns go into which views
-        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME};
-        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
-
-        // Create an empty adapter we will use to display the loaded data.
-        // We pass null for the cursor, then update it in onLoadFinished()
-/*
-        mAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                null,
-                fromColumns, toViews, 0);
-*/
-/*
-        String selection = ContactsContract.Contacts.DISPLAY_NAME + " <> \"\" ";
-        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
-                new String[] { ContactsContract.Contacts._ID,
-                        ContactsContract.Contacts.DISPLAY_NAME},
-                selection,
-                null,
-                "UPPER(" + ContactsContract.Contacts.DISPLAY_NAME + ") ASC");
-*/
         mAdapter = new CustomCursorAdapter0(getActivity().getApplicationContext(),null,0);
-/*
-        mAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                //android.R.layout.simple_list_item_multiple_choice,
-                R.layout.contacts_fragment_item,
-                null,
-                new String[] { Contacts.DISPLAY_NAME, Contacts.LOOKUP_KEY  },
-                new int[] { R.id.firstLine, R.id.secondLine },
-                //new int[] { android.R.id.text1 },
-                0);
-*/
-        // Prepare the loader.  Either re-connect with an existing one,
-        // or start a new one.
         getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         mSearchView = (SearchView)getActivity().findViewById(R.id.searchView);
         if( mSearchView != null ){ mSearchView.setOnQueryTextListener(this); }
     }
@@ -159,17 +94,6 @@ public class ContactsFragmentBase0 extends ListFragment
 
     // Called when a new Loader needs to be created
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
-/*
-        return new CursorLoader(getActivity(),
-                //ContactsContract.Data.CONTENT_URI,
-                ContactsContract.Contacts.CONTENT_URI,
-                PROJECTION,
-                SELECTION,
-                null,
-                ORDER);
-*/
         Uri baseUri;
         if (mCurFilter != null) {
             baseUri = Uri.withAppendedPath(Contacts.CONTENT_FILTER_URI,
@@ -178,8 +102,6 @@ public class ContactsFragmentBase0 extends ListFragment
             baseUri = Contacts.CONTENT_URI;
         }
 
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
         String select = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
                 + Contacts.HAS_PHONE_NUMBER + "='1') AND ("
                 + Contacts.DISPLAY_NAME + " != '' ))";
@@ -189,12 +111,18 @@ public class ContactsFragmentBase0 extends ListFragment
     }
 
     // Called when a previously created loader has finished loading
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        // Swap the new cursor in.  (The framework will take care of closing the
-        // old cursor once we return.)
-        View progress = (View) getActivity().findViewById(R.id.progress_bar);
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        String displayName, lookUpKey;
+        do {
+            displayName = cursor.getString(cursor.getColumnIndex(Contacts.DISPLAY_NAME));
+            lookUpKey = cursor.getString(cursor.getColumnIndex(Contacts.LOOKUP_KEY));
+            mAdapter.getHashData4(displayName,lookUpKey);
+        }while (cursor.moveToNext());
+
+        TextView progress = (TextView) getActivity().findViewById(R.id.progress_bar);
         if( progress != null ){ progress.setVisibility(View.INVISIBLE); }
-        mAdapter.swapCursor(data);
+        mAdapter.swapCursor(cursor);
     }
 
     // Called when a previously created loader is reset, making the data unavailable
--- app/src/main/java/com/awsomelink/db/adapters/CustomCursorAdapter0.java
+++ app/src/main/java/com/awsomelink/db/adapters/CustomCursorAdapter0.java
@@ -2,30 +2,81 @@ package com.awsomelink.db.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Data;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckedTextView;
 import android.widget.CursorAdapter;
-
+import android.widget.LinearLayout;
 import com.awsomelink.R;
+import java.util.HashMap;
+import java.util.Locale;
 
 /**
  * Created by m.nurullayev on 30.03.2015.
  */
 public class CustomCursorAdapter0 extends CursorAdapter {
     private LayoutInflater mLayoutInflater;
+    private static final String TAG = "CustomCursorAdapter0";
+    private static HashMap<String,HashMap<String,Boolean>> mDataHash = new HashMap<>();
+    private Context mContext ;
 
     public CustomCursorAdapter0(Context context, Cursor c, int flags) {
         super(context, c, flags);
+        mContext = context;
         mLayoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        CheckedTextView title = (CheckedTextView) view.findViewById(R.id.cb_title);
+        String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        if( title != null ) title.setText(displayName);
+        LinearLayout ll = (LinearLayout) view.findViewById(R.id.list_row_content);
+        if( ll != null ){
+            ll.removeAllViews();
+            String lookUpKey = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+            HashMap<String, Boolean> phones = getHashData4(displayName,lookUpKey);
+            for(String key: phones.keySet()){
+                CheckedTextView phone = (CheckedTextView) mLayoutInflater.inflate(R.layout.contacts_item_row_level2,null);
+                phone.setText(key);
+                ll.addView(phone);
+            }
+        }
+    }
+
+    public HashMap<String,Boolean> getHashData4(String displayName, String lookUpKey){
+        if( mDataHash.containsKey(displayName) ){
+            return(mDataHash.get(displayName));
+        }
+        Cursor pCur = mContext.getContentResolver().query(ContactsContract.Data.CONTENT_URI,
+                new String[] {Data._ID, Phone.NUMBER, Phone.TYPE, Phone.LABEL},
+                Data.LOOKUP_KEY + "=?" + " AND "
+                        + Data.MIMETYPE + "='" + Phone.CONTENT_ITEM_TYPE + "'",
+                new String[] {lookUpKey}, null);
+        int test = 0;
+        HashMap<String, Boolean> phones = new HashMap<>();
+        while (pCur.moveToNext())
+        {
+            String contactNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            if( !phones.containsKey(contactNumber) ) {
+                contactNumber = PhoneNumberUtils.formatNumber(contactNumber, Locale.getDefault().getCountry());
+                phones.put(contactNumber, Boolean.FALSE);
+                Log.d(TAG, "Contact " + String.valueOf(test++) + ": " + displayName + ", " + contactNumber);
+            }
+        }
+        pCur.close();
+        mDataHash.put(displayName, phones);
+        return(phones);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return mLayoutInflater.inflate(R.layout.contacts_fragment_item,parent,false);
+        return mLayoutInflater.inflate(R.layout.contacts_item_row,parent,false);
     }
 }
